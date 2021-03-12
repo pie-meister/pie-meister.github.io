ed0c40a544e1cf0981631b92f0269f7d27b17216
Danny Engelman <danny@engelman.nl>
2021-03-12 12:00:11 +0100

    v2

--- elements.pie-meister.js
+++ elements.pie-meister.js
@@ -112,7 +112,7 @@ customElements.define(
         let label = document.createElementNS(namespace, "text");
 
         // TODO: is it worth to have the path METHOD reference available for the outside world?
-        //group.path = path;
+        group.path = path;
         // group.p is middle of <path> strokeWidth draws the path
         //! Calculate center point once! Later calls would use other parameters
         group.p = path.M(); // sliceSize is a variable in scope
