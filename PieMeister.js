acae416f5fd2693214f5e0d4899b746be3f95512
Danny Engelman <danny@engelman.nl>
2021-03-15 20:02:41 +0100

    rest

--- PieMeister.js
+++ PieMeister.js
@@ -1,5 +1,5 @@
 // IDE VSCode with comment highlighting for comments (green), todo (orange), warning (red)
-// PieMeister 
+// PieMeister
 // #METHODS:
 // - svg(root) redraws the whole pie-chart from <slice> in lightDOM in specified root
 // - $ - array of all drawn <g> slices
@@ -10,19 +10,13 @@
 //            #PROPERTIES
 //            pulled = state
 //            #METHODS
-//            - path.point 
+//            - path.point
 //            - create ( extraRadius , )  creates new slice <path> Element (doesn't overwrite the current slice)
 //            - pull
 console.log("PieMeister 3.14");
 export default class PieMeister extends HTMLElement {
   connectedCallback() {
-    this.svg(
-      this.attachShadow({
-        // shadowDOM can only be attached once, user will see error in console
-        // todo: add constructor
-        mode: `open`,
-      })
-    );
+    this.svg();
   }
   // declared as method .svg so user can redraw a chart
   // this Component does not track for changed attributes (yet)
@@ -32,30 +26,36 @@ export default class PieMeister extends HTMLElement {
   //    <text>  slice label
   // </g>
   svg(
-    root,
-    // ================================================================================== optional user parameters
+    root = this.attachShadow({
+      // shadowDOM can only be attached once, user will see error in console
+      // todo: add constructor
+      mode: `open`,
+    }),
+    // =========================================================================== optional user parameters
     colors = (this.getAttribute`stroke` || `#e33,#3e3,#35e,#ee3,#e5e,#3ee`)
       .split`,`, //  #f2e  #962
     pull = ~~this.getAttribute`pull`, // how far a slice can be pulled outward
-    gap = ~~this.getAttribute`gap`, // gap between slices
-    // ================================================================================== configuration
+    //gap = Number(this.getAttribute`gap`), //! between slices, messes up % calcs
+    // =========================================================================== configuration
     // Component controlled variables (code golf, saves 4 bytes let statement)
     dashoffset = 0, // clockwise - incremental stroke offset for each slice
     //namespace = `http://www.w3.org/2000/svg`,
-    // ================================================================================== pathlength for all slices
+    // =========================================================================== pathlength for all slices
+    // todo add default offset path length
     pathLength = 0 // determined by first slice size, 100 for size=N% notation
   ) {
     // wait till DOM children are parsed
     setTimeout((_) => {
-      // ================================================================================== create SVG
+      // =========================================================================== create SVG
       // inject SVG in shadowDOM so all SVG is parsed and <slice> can be processed
       // default viewBox = 1000 x 1000 extra padding with pull attribute
       root.innerHTML = `<style>:host{display:inline-block}svg{width:100%}</style><svg xmlns=http://www.w3.org/2000/svg viewBox=0,0,${
         1000 + pull
       },${1000 + pull}>${this.innerHTML}</svg>`;
       // append user <slice> as HTML so we keep all <slice>
-      // ================================================================================== convert <slice>s to paths
-      this.$ = [...root.querySelectorAll`slice`].map((sliceDefinition, idx) => {
+      // =========================================================================== convert <slice>s to paths
+      //this.$ = [...root.querySelectorAll`slice`].map((sliceDefinition, idx) => {
+      [...root.querySelectorAll`slice`].map((sliceDefinition, idx) => {
         // <g> is the slice containing <path> and <text> label
         let group = document.createElementNS(`http://www.w3.org/2000/svg`, `g`);
         let label = document.createElementNS(
@@ -69,10 +69,10 @@ export default class PieMeister extends HTMLElement {
           ~~sliceDefinition.getAttribute`stroke-width` || // <slice stroke width=X>
           ~~this.getAttribute`stroke-width` || // <pie-chart stroke width=X>
           500 + pull / 2 - pull; // default Center + HALF pull margin - the pulled slice distance
-        //! store dashoffset for the START of this slice
+        //! slice START dashoffset
         let slice_dashoffset = dashoffset;
-
-        // ================================================================================== createPath
+        let path; // declare use later;
+        // =========================================================================== createPath
         group.create = (
           // user parameter
           extraRadius = 0, // 0 = slice middle point
@@ -83,7 +83,7 @@ export default class PieMeister extends HTMLElement {
             (extraRadius == 0 && colors.shift()),
           //! slice parameters
           __sliceSize = sliceSize,
-          __stroke_dashoffset = slice_dashoffset,
+          __stroke_dashoffset = slice_dashoffset, // todo parameteriz to earlier
 
           //! calculations, again:preventing the need for 4 Bytes let statement
           R = (500 + pull / 2) / 2 /* center point of SVG */ -
@@ -132,50 +132,42 @@ export default class PieMeister extends HTMLElement {
               : path.getPointAtLength(
                   path.getTotalLength() -
                     (path.getTotalLength() / pathLength) *
-                      (offset + (slice_dashoffset + __sliceSize / 2))
+                      (offset + (__stroke_dashoffset + __sliceSize / 2))
                 )),
           // return path, so it can be added to the slice <g> group
           // again ... avoiding the use for a return statement .. that is 6 Bytes man!
           path
         );
-        // ------------------------------------------------------------------ determine pathLength from lightDOM <slice>
+        // =========================================================== determine pathLength from lightDOM <slice>
         // first slice size=`N` sets pathlength for all other slices
         if (sliceSize == sizeString) {
           // NO % on first size=`N`
           if (!pathLength) {
             // calculate AND set the pathlength for No Percentage pie
             [...this.querySelectorAll`slice`].map(
-              (slice) => (pathLength += ~~slice.getAttribute`size` + gap)
+              //(slice) => (pathLength += ~~slice.getAttribute`size` + gap)
+              (slice) => (pathLength += ~~slice.getAttribute`size`)
             );
           }
         } else {
-          pathLength = ~~this.getAttribute`size` || 100; // 100% pie
+          pathLength = ~~this.getAttribute`size` || 100; // default 100% pie // todo move to parameter
         }
+        // ----------------------------------------------------------- calc remainder slice
         // size=`0` or size= `` calculates % remainder
         if (!sliceSize)
+          //(sliceSize = pathLength - dashoffset-gap), (sizeString = sliceSize + `%`);
           (sliceSize = pathLength - dashoffset), (sizeString = sliceSize + `%`);
 
-        // ------------------------------------------------------------------ create SVG slice
-        //! let defined above to save let statement
-        group.path = group.create(0); // path stroke positioning 0 = middle of slice
+        // ----------------------------------------------------------- create SVG slice
+        path = group.create(0); // path stroke positioning 0 = middle of slice
+        group.point = path.point;
 
-        //! 43 Bytes for Events
-        group.onmouseout = (_) => (
-          (this.slice = group),
-          this.dispatchEvent(new Event(`slice`), group.pull(0))
-        );
-        group.onmouseover = (_) => (
-          (this.slice = group),
-          this.dispatchEvent(new Event(`slice`), group.pull(1))
-        );
-
-        // set extra properties on slice <g> for easy CSS selecting
-        // group.setAttribute(`sw`, strokeWidth);
-        // group.setAttribute(`offset`, dashoffset);
+        // ------------------------------------------------------------------extra <g> attributes
+        group.setAttribute(`slice`, idx);
         group.setAttribute(`size`, sizeString);
-        group.setAttribute(`slice`, idx + 1);
         group.setAttribute(
-          `text`,
+          `label`,
+          //! if a <style> is defined show a Label
           (label.innerHTML = this.querySelector`style` // set the svg innerHTML
             ? /* if sliceBase has a label */ (sliceDefinition.innerHTML &&
                 /* then replace % */ sliceDefinition.innerHTML.replace(
@@ -183,23 +175,23 @@ export default class PieMeister extends HTMLElement {
                   sizeString
                 )) ||
               /* else use: */ sizeString
-            : ``)
+            : /* empty innerHTML string */ ``)
         );
-        //! calculate dashoffset ONCE for each slice, I tried to parameterize to for the create Slice function
-        //! but need way more code. This means 3rd party code needs to substract the size from the offset see example
-        group.path.setAttribute(
+        // ----------------------------------------------------------- calc NEXT slice dashoffset ONCE
+        path.setAttribute(
           `stroke-dashoffset`,
-          (dashoffset += sliceSize + gap)
+          // (dashoffset += sliceSize + gap)
+          (dashoffset += sliceSize)
         );
-        // ------------------------------------------------------------------ add user <slice> attributes
+        // ----------------------------------------------------------- add user <slice> attributes
         [...sliceDefinition.attributes].map((x) =>
-          group.path.setAttribute(x.name, x.value)
+          path.setAttribute(x.name, x.value)
         );
-        // ------------------------------------------------------------------ set <text x y> position
+        // ----------------------------------------------------------- set <text x y> position
         // textPoint used for <text> label position calculation, not drawn on SVG
         label.setAttribute(
           `y`,
-          group.path.point(
+          group.point(
             0,
             ~~sliceDefinition.getAttribute`pulltext` ||
               ~~this.getAttribute`pulltext` ||
@@ -208,33 +200,29 @@ export default class PieMeister extends HTMLElement {
         );
         label.setAttribute(
           `x`,
-          group.path.point(
+          group.point(
             0,
             ~~sliceDefinition.getAttribute`pulltext` ||
               ~~this.getAttribute`pulltext` ||
               0
           ).x
         );
-        // ------------------------------------------------------------------ add path and label to <group>
-        group.append(group.path, label);
+        // ----------------------------------------------------------- add path and label to <group>
+        group.append(path, label);
         // parentNode can be SVG or user element <g>
         sliceDefinition.parentNode.replaceChild(group, sliceDefinition);
-        // ------------------------------------------------------------------ create method .pull(T/F)
-        group.pull = (
-          state = sliceDefinition.hasAttribute`pull`,
-          pt = group.path.point(0, Math.abs(~~sliceDefinition.getAttribute`pull` || pull))
-        ) =>
+        let pt = group.point(
+          0,
+          Math.abs(~~sliceDefinition.getAttribute`pull` || pull)
+        );
+        if (sliceDefinition.hasAttribute`pull`) {
           group.setAttribute(
             `transform`,
-            (group.pulled = state) // SET pulled property 
-              ? `translate(${
-                  pt.x - group.path.point().x
-                } ${pt.y - group.path.point().y})`
-              : `` //`translate(0 0)`
+            `translate(${pt.x - group.point().x} ${pt.y - group.point().y})`
           );
-        // ------------------------------------------------------------------ pull current slice
-        group.pull();
-        return group;
+        }
+        // ----------------------------------------------------------- pull current slice
+        // return group;
       }); // convert all slices
     });
   } // svg
